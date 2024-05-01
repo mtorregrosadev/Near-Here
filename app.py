4f44cca80e4f3495ddf3aced62e345a5e7c5c3fa
The_Roboters <TheRoboterss@gmail.com>
2024-05-01 12:32:50 +0200

    Updateee (Por a perdre tot en vrt)
    Val, actualitzacions fins ara:
    (Volia actualitzar-ho per si li passava alguna cosa a l'ordinador)
    
    - Feta una app responsiva al 100% (literalment al 100%), agafa tot l'espai que té disponible i fa percentatges per a cadascun d'ells, acomodant els contenidors, els botons i tot. Amb això he esborrat el bt el qual feia que estigués a sota. Fent l'app responsiva ja s'acomodava tot on volia.
    
    - Canviat la font a Lato, de moment la deixo així, però segurament la canviaré a un altre. No n'estic segur
    
    - Vull fer dimissible (per moure els contenidors), per això l'he importat a dalt, encara està en procés.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import flet 
-from flet import Page, ElevatedButton, SafeArea,ScrollbarTheme,Theme,MaterialState, animation, Container, transform, Icon, icons, colors,Divider, alignment, IconButton, icons, Row, Text, ResponsiveRow, RoundedRectangleBorder, Chip, TextField, NavigationDestination, NavigationBar,NavigationBarLabelBehavior 
+from flet import Page, ElevatedButton, Dismissible,DismissDirection, SafeArea,Theme, animation, Container, transform, Icon, icons, colors, alignment, icons, Row, Text, ResponsiveRow, Chip, NavigationDestination, NavigationBar 
 import time
 from math import pi
 
@@ -8,14 +8,19 @@ def main(page: Page):
     page.window_width = 600
     page.window_height = 800
     page.horizontal_alignment = "center"
-    #page.vertical_alignment ="end"
     page.theme_mode = "light"
-
     page.fonts = {
            "Outfit": "fonts/Outfit.ttf",
+           "Lato": "fonts/Lato.ttf",
     }
-    page.theme = Theme(font_family="Outfit")
+    page.theme = Theme(font_family="Lato")
     page.update()
+
+    def animate_movement(e):
+        if e.direction == DismissDirection.END_TO_START: #De dreta a esquerra 
+            print("dreta")
+        else: #De esquerra a dreta
+            print("esquerra")
 
     def animate_left(e):
         if contenidor2.visible == False:
@@ -96,7 +101,6 @@ def main(page: Page):
     def restaurants_select(e):
         pass
     
-    divider = Divider(height=30, thickness=0.1)
     
 #Definirem aqui tots els components com a variables per a tal d'accedir-hi en qualsevol moment en el programa
     Tags_amunt =Row( #Totes les etiquetes juntes 
@@ -139,10 +143,8 @@ def main(page: Page):
             )
     contenidors = ResponsiveRow( #Aqui van tots els contenidors junts 
             offset=(0,0),
-            expand = 18,
+            expand = True,
             expand_loose=True,
-            height=560,
-            width=500,
             controls=[
                 contenidor,
                 contenidor2,
@@ -150,12 +152,12 @@ def main(page: Page):
         )
     botons = ResponsiveRow( #Aqui van tots els botons junts 
             vertical_alignment="end",
+            
             controls=[
-            ElevatedButton("Seguent", on_click=animate_left, bgcolor="#d9acaa", col=4), 
-            ElevatedButton("Més info", on_click=animate_center,bgcolor="#FBF9F1",col=4),
-            ElevatedButton("Guarda!", on_click=animate_right, bgcolor="#aad9c4",col=4), 
+                ElevatedButton("Seguent", on_click=animate_left, bgcolor="#d9acaa", color="black",col=4), 
+                ElevatedButton("Més info", on_click=animate_center,bgcolor="#FBF9F1",color="black",col=4),
+                ElevatedButton("Guarda!", on_click=animate_right, bgcolor="#aad9c4",color="black",col=4), 
     ]) 
-    bt = Container(botons, bgcolor="red", expand=6)
 
     def changetab(e):
         index = e.control.selected_index
@@ -190,9 +192,6 @@ def main(page: Page):
     
     page.navigation_bar=NavigationBar(
         bgcolor = "#AAD7D9",
-        adaptive=True,
-        height=90,
-        #label_behavior=NavigationBarLabelBehavior.ONLY_SHOW_SELECTED,
         selected_index = 1,
         indicator_color = "#FBF9F1",
         on_change=changetab,
@@ -203,10 +202,19 @@ def main(page: Page):
         ]
     )
     
+    #contenidors.height = page.height * 0.8
+    #contenidors.width = page.width * 1
+    botons.height = page.height * 0.08
+    botons.width = page.width * 1
+    page.navigation_bar.height = page.height * 0.11
+    Tags_amunt.height = page.height * 0.05
+    Tags_amunt.width = page.width * 1
+
+
     page.add(
         SafeArea(content=Tags_amunt, top=True),
-        contenidors,
-        bt
+        contenidors, 
+        botons
     )
 
 flet.app(target=main,assets_dir="assets")
